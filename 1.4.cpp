6e317388310927b550ed05e1f41ec206533ad968
Anuj Bansal <bansalanuj1996@gmail.com>
2015-11-09 20:25:37 +0530

    Added 1.4

---
+++ 1.4.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include<sstream>
+using namespace std;
+
+void replace(string &s1, int l){
+	int newLenght, i,spaces=0;
+	for(i=0;i<l;i++){
+		if(s1[i] == ' ')
+			spaces++;
+	}
+	newLenght = spaces*2 + l;
+	s1[newLenght] = '\0';
+	for(i=l-1;i>0;i--){
+		if(s1[i] == ' '){
+			s1[newLenght-1] = '0';
+			s1[newLenght-2] = '2';
+			s1[newLenght-3] = '%';
+			newLenght = newLenght-3;
+		}
+		else{
+			s1[newLenght-1] = s1[i];
+		}
+	}
+}
+
+int main(){
+	//char s[256];
+	string s1;
+	int l;
+	getline(cin, s1);
+	stringstream myStream(s1);
+	myStream >> l;
+	getline(cin, s1);
+	//cout << s1.length() << endl;
+	//cout << s1 << endl;
+	replace(s1, l);
+	cout << s1;
+}
